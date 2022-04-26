5c710c7e896e60d736b6ac8b5fc9b6774c732e5c
ydjabela <ydjabela@blendbow.com>
2022-04-27 00:09:18 +0200

    update

--- controller/controller.py
+++ controller/controller.py
@@ -4,6 +4,24 @@ from model.database_actions import Database
 
 class MainMenu(Database):
 
+    #  ----------------------------------------------------------------------------------------------------------------#
+
+    def total_benifits(self, select_table, select_action_id):
+        total_binef = 0
+        for i in select_action_id:
+            total_binef += select_table[i]["benefits"]
+        return total_binef
+
+    #  ----------------------------------------------------------------------------------------------------------------#
+
+    def total_cost(self, select_table, select_action_id):
+        total_cst = 0
+        for i in select_action_id:
+            total_cst += select_table[i]["cost"]
+        return total_cst
+
+    #  ----------------------------------------------------------------------------------------------------------------#
+
     def menu(self):
         # init
         # update benifts
@@ -13,8 +31,8 @@ class MainMenu(Database):
         for i in range(len(select_table)):
             benefits_list.append(select_table[i]["benefits"])
         print(benefits_list)
-        couts_total_action = 0
-        while couts_total_action <=500:
-            break
 
+        total_binef = self.total_benifits(select_table=select_table, select_action_id=[1, 2, 3, 4, 5])
+        print(total_binef)
+        
 #   -------------------------------------------------------------------------------------------------------------------#
