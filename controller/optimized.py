792b58f87d947f961d445c6993977e9ee0deaa31
ydjabela <ydjabela@blendbow.com>
2022-05-09 11:10:15 +0200

    update

--- controller/optimized.py
+++ controller/optimized.py
@@ -1,6 +1,59 @@
-from model.database_actions import Database
-from model.csv_data import CsvData
 import time
+import csv
+from tinydb import TinyDB, Query
+
+# ---------------------------------------------------------------------------------------------------------------------#
+
+
+class Database:
+    @staticmethod
+    def database_action(table):
+        # "Actions_details" and result_details
+        db = TinyDB('actions.json')
+        table = db.table(table)
+        select_table = table.all()
+        return select_table, table
+
+    # ---------------------------------------------------------------------------------------------------------------------#
+
+    @staticmethod
+    def update_database(table="Actions_details", serialized=None):
+        db = TinyDB('actions.json')
+        table = db.table(table)
+        select_table = table.all()
+        actions = Query()
+        if table == "Actions_details":
+            # update binefits
+            for i in range(len(select_table)):
+                value = float(select_table[i]["cost"])*float(select_table[i]["percentage"])/100
+                action_nom_selected = select_table[i]['name']
+                print("Benefits:", action_nom_selected, "est de :", value)
+                table.update({"benefits": value}, actions.name == action_nom_selected)
+        else:
+            table.truncate()
+            table.insert(serialized)
+
+
+#  --------------------------------------------------------------------------------------------------------------------#
+
+
+class CsvData:
+    @staticmethod
+    def csvdata_actions(table):
+        # Ouvrir le fichier csv
+        select_table = list()
+        with open('dataset{}_Python+P7.csv'.format(table), 'r') as f:
+            # Créer un objet csv à partir du fichier
+            obj = csv.reader(f)
+            for ligne in obj:
+                select_table.append({
+                    'name': ligne[0],
+                    'price': ligne[1],
+                    'profit': ligne[2]
+                })
+            del select_table[0]
+        return select_table
+
 
 #  --------------------------------------------------------------------------------------------------------------------#
 
@@ -82,3 +135,10 @@ class Optimized(Database, CsvData):
             print('\033[93m' + "temps d'excution est de : {}".format(time_excution) + "\x1b[0m")
 
 #   -------------------------------------------------------------------------------------------------------------------#
+
+
+# optimized file
+Optimized().excution_optimized()
+
+# optimized sienna file
+Optimized().excution_optimized_sienna_data()
